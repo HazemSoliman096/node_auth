bcf997299f74bf5cd7211126a260f8745829fd96
Hazem Abdel-aziz <hazemsoliman096@gmail.com>
2022-11-14 17:07:52 +0200

    session init

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require('express');
+const session = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
 
-const middlewares = require('./middlewares/middlewares');
-const errorHandler = require('./middlewares/errorHandlers');
-
+const prisma = new PrismaClient();
+const sessionStore = new PrismaSessionStore(
+  prisma,
+  {
+    checkPeriod: 2 * 60 * 1000,
+    dbRecordIdIsSessionId: true,
+    dbRecordIdFunction: undefined,
+  }
+);
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.use(middlewares.middleware1);
-app.use(middlewares.middleware2);
+app.use(session({
+  secret: 'somsecret',
+  resave: false,
+  saveUninitialized: true,
+  store: sessionStore,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,
+  },
+}));
 
-app.get('/', (req, res, next) => {
-  console.log('I\'m standard callback');
-  res.send(`<h1>The value is ${req.customProperty}</h1>`);
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World Session</h1>')
 });
 
-app.use(errorHandler.errorHandler);
-
-app.listen(3000); 
+app.listen(3000);
